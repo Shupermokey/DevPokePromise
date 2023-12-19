f05bd232165d77d5a1a7339950b621c058d2720e
Shupermokey <a.e.sobieraj@gmail.com>
2023-12-19 15:53:33 -0600

    dry principle updates

--- script.js
+++ script.js
@@ -5,55 +5,58 @@ const searchTypeInput = document.getElementById("searchType-input");
 const searchTypeButton = document.getElementById("searchType-button");
 const sortAlpha = document.querySelector(".sort-alpha");
 const sortZeta = document.querySelector(".sort-zeta");
-let scroller = document.querySelector(".scroll-container")
-const scroll = document.querySelector(".scroller")
+let scroller = document.querySelector(".scroll-container");
+const scroll = document.querySelector(".scroller");
 
 let pokemonData = [];
 
 const mapEmAll = new Map();
 
-const fetchPokemon = () => {
+const getPokeArr = (num) => {
   const promises = [];
-  for (let i = 1; i <= 30; i++) {
+  for (let i = 1; i <= num; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     promises.push(fetch(url).then((res) => res.json()));
   }
-  Promise.all(promises).then((results) => {
-    
-    pokemonData = results.map((result) => (
-      {
-      name: result.name,
-      image: result.sprites["front_default"],
-      type: result.types.map((type) => type.type.name).join(" · "),
-      id: result.id,
-      fav: "",
-      star: "hollow",
-      img: "",
-      title: "",
-      sub: "",
-      active: "",
-    }
-    ));
-    createPokemonDataMap(pokemonData);
-    displayPokemon(pokemonData);
+  return promises;
+};
 
+const pokeArray = getPokeArr(30);
+
+const fetchPokemon = (pokeArray) => {
+  Promise.all(pokeArray).then((results) => {
+    buildUI(results);
   });
 };
 
+const buildUI = (data) => {
+  pokemonData = data.map((result) => ({
+    name: result.name,
+    image: result.sprites["front_default"],
+    type: result.types.map((type) => type.type.name).join(" · "),
+    id: result.id,
+    fav: "",
+    star: "hollow",
+    img: "",
+    title: "",
+    sub: "",
+    active: "",
+  }));
+  createPokemonDataMap(pokemonData);
+  displayPokemon(pokemonData);
+};
 
 function createPokemonDataMap(input) {
-  
   input.forEach((pokemon) => mapEmAll.set(pokemon.name, pokemon));
-
 }
-
-
 
 function createListElement(pokemon) {
   return pokemon
     .map((pokemon) => {
       const pokemonList = `<li class="${pokemon.name} card ${pokemon.active}">
-            <div class="star ${pokemon.fav}">${pokemon.star === "hollow" ? "☆" : "⭐" }</div>
+            <div class="star ${pokemon.fav}">${
+        pokemon.star === "hollow" ? "☆" : "⭐"
+      }</div>
             <img class="card-image ${pokemon.img}" src="${pokemon.image}"/>
             <p class="card-id">#${pokemon.id}</p>
             <h2 class="card-title ${pokemon.title}">${pokemon.name}</h2>
@@ -64,9 +67,20 @@ function createListElement(pokemon) {
     .join("");
 }
 
+const addStarListeners = (pMap, data) => {
+  data.addEventListener("click", () => {
+    if (data.innerHTML === "☆") {
+      data.innerHTML = "⭐";
+      pMap.star =  "fill";
+    } else {
+      data.innerHTML = "☆";
+      pMap.star = "hollow";
+    }
+  });
+}
 
 const addPokemonCardListeners = () => {
-  const pokemonCards = document.querySelectorAll(".card"); 
+  const pokemonCards = document.querySelectorAll(".card");
 
   pokemonCards.forEach((pokemon) => {
     const title = pokemon.querySelector(".card-title");
@@ -84,90 +98,68 @@ const addPokemonCardListeners = () => {
         sub.classList.toggle("card-subRevealed");
         star.classList.add("star-toggle");
         pokemon.classList.add("active");
-        
 
-        pMap.img = "card-imageRevealed"
-        pMap.title = "card-titleRevealed"
-        pMap.sub = "card-subRevealed"
-        pMap.active = "active"
-        pMap.star = "hollow"
-        pMap.fav = "star-toggle"
-        
+        pMap.img = "card-imageRevealed";
+        pMap.title = "card-titleRevealed";
+        pMap.sub = "card-subRevealed";
+        pMap.active = "active";
+        pMap.star = "hollow";
+        pMap.fav = "star-toggle";
       }
       displayScroller(mapEmAll);
-
     });
 
-    star.addEventListener("click", () => {
-      if (star.innerHTML === "☆") {
-        star.innerHTML = "⭐";
-        pMap.star = "fill"
-      } else {
-        star.innerHTML = "☆";
-        pMap.star = "hollow"
-      }
-    });
+    addStarListeners(pMap, star);
+
   });
-  };
-
+};
 
 function displayScroller(input) {
   scroll.removeChild(scroller);
-  scroller =  document.createElement("div");
-  scroller.classList.add('scroller-container');
+  scroller = document.createElement("div");
+  scroller.classList.add("scroller-container");
 
-  for(let i of input){
+  for (let i of input) {
     const pokemonC = document.createElement("div");
-    pokemonC.classList.add('pokedexContainer');
+    pokemonC.classList.add("pokedexContainer");
 
     const pokeBall = document.createElement("div");
-    if(i[1].star === "hollow"){
-      pokeBall.classList.add('pokeball');
-
-    }
-    else if(i[1].star === "fill") {
-      pokeBall.classList.add('pokeball');
-      pokeBall.classList.add('pokeemall');
-
+    if (i[1].star === "hollow") {
+      pokeBall.classList.add("pokeball");
+    } else if (i[1].star === "fill") {
+      pokeBall.classList.add("pokeball");
+      pokeBall.classList.add("pokeemall");
     }
 
     const pokemon = document.createElement("div");
-    pokemon.classList.add('pokemonName');
-    if(i[1].active === ""){
-      pokemon.innerHTML = "- - - - - - - -"
-    }
-    else {
+    pokemon.classList.add("pokemonName");
+    if (i[1].active === "") {
+      pokemon.innerHTML = "- - - - - - - -";
+    } else {
       pokemon.innerHTML = i[0];
     }
 
-    
     pokemonC.appendChild(pokeBall);
     pokemonC.appendChild(pokemon);
     scroller.appendChild(pokemonC);
     scroll.appendChild(scroller);
   }
-  
 }
-
-
 
 const displayPokemon = (pokemon) => {
-    pokedex.innerHTML = createListElement(pokemon);
-    addPokemonCardListeners();
-    displayScroller(mapEmAll);
-  
-}
-
-
+  pokedex.innerHTML = createListElement(pokemon);
+  addPokemonCardListeners();
+  displayScroller(mapEmAll);
+};
 
 const searchPokemon = (event) => {
-  event.preventDefault();
   const searchTerm = searchInput.value.toLowerCase();
 
   if (searchTerm === "") {
-    pokemonData.sort(sortNumerical);
+    displayPokemon(doSorting(pokemonData, "id", 'asc'));
   } else {
-    pokemonData.sort(comparePokemonId)
+    // pokemonData.sort(comparePokemonId);
+    doSorting(pokemonData, "id", 'asc');
     const filteredPokemon = pokemonData.filter((pokemon) =>
       pokemon.name.includes(searchTerm)
     );
@@ -176,60 +168,78 @@ const searchPokemon = (event) => {
 };
 
 const searchPokemonType = (event) => {
-  event.preventDefault();
   const searchType = searchTypeInput.value.toLowerCase();
 
   if (searchType === "") {
-    pokemonData.sort(sortNumerical);
-
+    displayPokemon(doSorting(pokemonData, "id", 'asc'));
   } else {
-    pokemonData.sort(comparePokemonId)
+    // pokemonData.sort(comparePokemonId);
+    doSorting(pokemonData, "id", 'asc');
+
     const filterType = pokemonData.filter((pokemon) =>
       pokemon.type.includes(searchType)
     );
-    console.log(filterType);
     displayPokemon(filterType);
   }
 };
 
-function comparePokemon(a, b) {
-  if (a.name < b.name) {
-    return -1;
-  } else if (a.name > b.name) {
-    return 1;
-  }
-  return 0;
-}
-function comparePokemonButTheOtherWay(a, b) {
-  if (a.name > b.name) {
-    return -1;
-  } else if (a.name < b.name) {
-    return 1;
-  }
-  return 0;
+
+const doSorting = (data, prop, dir) => {
+  return data.sort((a,b) => {
+    const aVal = a[prop]
+    const bVal = b[prop]
+    if(aVal < bVal) {
+      return dir === 'asc' ? -1 : 1;
+    }
+    else if(aVal > bVal) {
+      return dir === 'asc' ? 1 : -1;
+    }
+    return 0;
+  })
 }
 
-function comparePokemonId(a, b) {
-  if (a.id < b.id) {
-    return -1;
-  } else if (a.id > b.id) {
-    return 1;
-  }
-  return 0;
-}
+// function comparePokemon(a, b) {
+//   if (a.name < b.name) {
+//     return -1;
+//   } else if (a.name > b.name) {
+//     return 1;
+//   }
+//   return 0;
+// }
+// function comparePokemonButTheOtherWay(a, b) {
+//   if (a.name > b.name) {
+//     return -1;
+//   } else if (a.name < b.name) {
+//     return 1;
+//   }
+//   return 0;
+// }
+
+// function comparePokemonId(a, b) {
+//   if (a.id < b.id) {
+//     return -1;
+//   } else if (a.id > b.id) {
+//     return 1;
+//   }
+//   return 0;
+// }
 
 const sortAlphabetical = (event) => {
   event.preventDefault();
-  displayPokemon(pokemonData.sort(comparePokemon));
+  displayPokemon(doSorting(pokemonData, "name", "asc"));
+
 };
 
 const sortZetanumeril = (event) => {
   event.preventDefault();
-  displayPokemon(pokemonData.sort(comparePokemonButTheOtherWay));
+  displayPokemon(doSorting(pokemonData, "name", "dec"));
+
 };
 
 const sortNumerical = (event) => {
-  displayPokemon(pokemonData.sort(comparePokemonId));
+  event.preventDefault();
+  displayPokemon(doSorting(pokemonData, "id", "asc"));
+
 };
 
 searchButton.addEventListener("click", searchPokemon);
@@ -240,6 +250,4 @@ sortAlpha.addEventListener("click", sortAlphabetical);
 
 sortZeta.addEventListener("click", sortZetanumeril);
 
-fetchPokemon();
-
-
+fetchPokemon(pokeArray);
